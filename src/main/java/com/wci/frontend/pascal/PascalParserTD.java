d14e6df8307960cb9fd92848832bc96f4c4788dd
gaurav <gdhameeja@gmail.com>
2018-09-27 00:25:59 +0530

    I need to get more serious about this

--- src/main/java/com/wci/frontend/pascal/PascalParserTD.java
+++ src/main/java/com/wci/frontend/pascal/PascalParserTD.java
@@ -2,71 +2,75 @@
 
 package com.wci.frontend.pascal;
 
+import static com.wci.message.MessageType.PARSER_SUMMARY;
+
 import com.wci.frontend.*;
 import com.wci.message.*;
 
-import static com.wci.message.MessageType.PARSER_SUMMARY;
-
 /**
- * <h1> PascalParserTD </h1>
- * <p> The top-down Pascal parser. </p>
+ *
+ *
+ * <h1>PascalParserTD </h1>
+ *
+ * <p>The top-down Pascal parser.
  */
+public class PascalParserTD extends Parser {
 
-public class PascalParserTD extends Parser{
+  /**
+   * Constructor.
+   *
+   * @param scanner the scanner to be used with this parser.
+   */
+  public PascalParserTD(Scanner scanner) {
+    super(scanner);
+  }
 
-	/**
-	 * Constructor.
-	 * @param scanner the scanner to be used with this parser.
-	 */
-	public PascalParserTD(Scanner scanner){
-		super(scanner);
-	}
+  /** Parse a pascal source program and generate the intermediate code and symbol table. */
+  public void parse() throws Exception {
+    Token token;
+    long startTime = System.currentTimeMillis();
+    try {
+      // loop over each token until end of file.
+      while (!((token = nextToken()) instanceof EofToken)) {
+        TokenType tokenType = token.getType();
 
-	/**
-	 * Parse a pascal source program and generate the intermediate code and symbol table.
-	 */
-	public void parse() throws Exception{
-		Token token;
-		long startTime = System.currentTimeMillis();
-		try{
-		  // loop over each token until end of file.
-		  while(!((token = nextToken()) instanceof EofToken)){
-		    TokenType tokenType = token.getType();
+        if (tokenType != ERROR) {
 
-		    if(tokenType != ERROR){
+          // Format each token.
+          sendMessage(
+              new Message(
+                  TOKEN,
+                  new Object[] {
+                    token.getLineNum(),
+                    token.getPosition(),
+                    tokenType,
+                    token.getText(),
+                    token.getValue()
+                  }));
+        } else {
+          errorHandler.flag(token, (PascalErrorCode) token.getValue(), this);
+        }
+      }
 
-		      // Format each token.
-		      sendMessage(new Message(TOKEN, new Object[] {
-			    token.getLineNum(),
-			    token.getPosition(),
-			    tokenType,
-			    token.getText(),
-			    token.getValue()
-			  }));
-		    }
-		    else{
-		      errorHandler.flag(token, (PascalErrorCode) token.getValue(), this)
-		    }
-		}
+      // Send the parser summary message.
+      float elapsedTime = (System.currentTimeMillis() - startTime) / 1000f;
+      // 1000f becuase 1 sec = 1000 millisec;so dividing by 1000f will give us time in seconds
+      // (floating point number).
 
-		// Send the parser summary message.
-		float elapsedTime = (System.currentTimeMillis() - startTime)/1000f;
-		// 1000f becuase 1 sec = 1000 millisec;so dividing by 1000f will give us time in seconds (floating point number).
+      sendMessage(
+          new Message(
+              PARSER_SUMMARY, new Number[] {token.getLineNum(), getErrorCount(), elapsedTime}));
+    } catch (java.io.IOException ex) {
+      errorHandler.abortTranslation(IO_ERROR, this);
+    }
+  }
 
-		sendMessage(new Message(PARSER_SUMMARY, new Number[] {
-				token.getLineNum(), getErrorCount(), elapsedTime
-		}));
-		}
-		catch(java.io.IOException ex){
-		  errorHandler.abortTranslation(IO_ERROR, this);
-		}
-	}
-
-	/**
-	 * Return the number of syntax errors found by the parser.
-	 * @return the error count.
-	 */
-	public int getErrorCount(){
-	  return errorHandler.getErrorCount();
-	}
+  /**
+   * Return the number of syntax errors found by the parser.
+   *
+   * @return the error count.
+   */
+  public int getErrorCount() {
+    return errorHandler.getErrorCount();
+  }
 }
--- src/main/java/com/wci/frontend/pascal/PascalScanner.java
+++ src/main/java/com/wci/frontend/pascal/PascalScanner.java
@@ -1,39 +1,42 @@
 package com.wci.frontend.pascal;
 
-import com.wci.frontend.*;
-
 import static com.wci.frontend.Source.EOF;
 
+import com.wci.frontend.*;
+
 /**
- * <h1> PascalScanner </h1>
- * <p> The Pascal scanner. </p> 
+ *
+ *
+ * <h1>PascalScanner </h1>
+ *
+ * <p>The Pascal scanner.
  */
+public class PascalScanner extends Scanner {
+  /**
+   * Constructor
+   *
+   * @param source the source to be used with this scanner.
+   */
+  public PascalScanner(Source source) {
+    super(source);
+  }
 
-public class PascalScanner extends Scanner{
-	/**
-	 * Constructor
-	 * @param source the source to be used with this scanner.
-	 */
-	public PascalScanner(Source source){
-		super(source);
-	}
+  /**
+   * Extract and return the next Pascal token from the source.
+   *
+   * @return the next token.
+   * @throws Exception if error occurred.
+   */
+  public Token extractToken() throws Exception {
+    Token token;
 
-	/**
-	 * Extract and return the next Pascal token from the source.
-	 * @return the next token.
-	 * @throws Exception if error occurred.
-	 */
-	public Token extractToken() throws Exception{
-		Token token;
-
-		// Construct the next token. The current character determines the token type.
-		char currentChar = currentChar();
-		if(currentChar == EOF){
-			token = new EofToken(source);
-		}
-		else{
-			token = new Token(source);
-		}
-		return token;
-	}
+    // Construct the next token. The current character determines the token type.
+    char currentChar = currentChar();
+    if (currentChar == EOF) {
+      token = new EofToken(source);
+    } else {
+      token = new Token(source);
+    }
+    return token;
+  }
 }
